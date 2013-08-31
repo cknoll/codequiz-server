c77cc37aa0df6cbde724886c13f93f16e2903ebc
cknoll <CarstenKnoll@gmx.de>
2013-08-31 13:43:35 +0200

    save status quo before rewriting the templates

---
+++ aux_scripts/template_name.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Intention: go through all html files in the working dir
+and add comment lines at the beginning and end containing the name of the
+(template) file
+"""
+
+import os
+
+files  = os.listdir('.')
+files = filter(lambda x: x.endswith('.html'), files)
+
+def add_comments(fname):
+    with open(fname, 'r') as myfile:
+        lines = myfile.readlines()
+
+    c1 = "<!-- ==> start: %s -->\n" % (fname)
+    c2 = "<!-- ==> end: %s -->\n" % (fname)
+
+    lines.insert(0,c1)
+    lines.append(c2)
+
+    if 0:
+        print lines[0]
+        print lines[-1]
+        return
+    with open(fname, 'w') as myfile:
+        myfile.writelines(lines)
+
+for f in files:
+    print f
+    add_comments(f)
+
+print "done"
