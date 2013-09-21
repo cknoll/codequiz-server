e6489625df0e3a20f32065f2da05a020242bad9f
Pit Garbe <leberwurstsaft@me.com>
2013-09-21 14:37:28 +0200

    Don't use absolute path in settings

--- codequiz-server/codequiz/settings.py
+++ codequiz-server/codequiz/settings.py
@@ -107,8 +107,16 @@ ROOT_URLCONF = 'codequiz.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'codequiz.wsgi.application'
 
+# Don't use absolute paths, build up the path instead. This is better for collaboration,
+# as not everybody has the same file structure
+
+import os
+
+path = os.path.abspath("views.py")
+dir_path = os.path.dirname(path)
+
 TEMPLATE_DIRS = (
-    "/home/ck/projekte/inkubator/py_CodeQuiz/codequiz/templates/",
+    dir_path + "templates",
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
