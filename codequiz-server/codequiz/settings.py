be9942e47787c1e1bff6c346862a70d98d774c60
Pit Garbe <leberwurstsaft@me.com>
2013-10-07 16:45:03 +0200

    restored sample tasks

--- codequiz-server/codequiz/settings.py
+++ codequiz-server/codequiz/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'codequiz_db_file_2',                      # Or path to database file if using sqlite3.
+        'NAME': 'codequiz_db_file',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
