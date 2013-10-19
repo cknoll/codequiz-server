e7655100057bfa4ddfabc0069018b054e5f21682
Pit Garbe <leberwurstsaft@me.com>
2013-10-19 11:30:20 +0200

    First try at inserting the task builder in a custom admin template
    Doesn't yet save anything. Only fills a hidden input with the generated JSON.

--- codequiz-server/quiz/admin.py
+++ codequiz-server/quiz/admin.py
@@ -1,6 +1,9 @@
 # coding=utf-8
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import permission_required
+from django.conf.urls import *
+import admin_views as views
 
 from django import forms
 from django_ace import AceWidget
@@ -34,41 +37,30 @@ class TaskAdmin(admin.ModelAdmin):
     includes a filter by author, more meta data in the list view, hides unnecessary fields,
     replaces the text field element with a sophisticated code editor
     """
-    list_display = ['id', 'title', 'pub_date', 'revision']
-    list_display_links = ['title']
-    search_fields = ['title', 'tags']
-    date_hierarchy = 'pub_date'
-    inlines = (TC_MembershipInline,)
-    list_filter = ['author']
+    add_form_template = "admin/admin_add_view.html"
+    change_form_template = "admin/admin_change_view.html"
 
-    fieldsets = [
-        (None, {'fields': ['title', 'body_xml', 'tags']}),
-        ('Date', {'fields': ['pub_date'],
-                  'description': 'Wird automatisch auf aktuelle Zeit gesetzt, kann aber geändert werden',
-                  'classes': ('collapse',)}),
-    ]
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        #extra_context['osm_data'] = self.get_osm_info()
+        extra_context['task'] = Task.objects.get(id=object_id)
+        return super(TaskAdmin, self).change_view(request, object_id, form_url, extra_context=extra_context)
 
-    # comment the following line in case of problems with Java-Script editor enhancement
-    form = TaskAdminForm
-
-    def save_model(self, request, task, form, change):
-        """
-        hooks into saving a task to add/change some data automatically
-
-        specifically, the author gets set to the logged in users name/username
-        and the tasks revision is incremented
-        """
-        if not task.author:
-            user = User.objects.get(username__exact=request.user)
-            name = user.get_full_name()
-            if not name:
-                name = user.get_short_name()
-                if not name:
-                    name = user.get_username()
-
-            task.author = name
-        task.revision += 1
-        task.save()
+    #def add_view(self, request):
+    #    # View for an add request
+    #    return views.admin_add_view(request, self)
+    #
+    #def change_view(self, request, taskid):
+    #    # View for a change request
+    #    return views.admin_change_view(request, self, taskid)
+    #
+    #def get_urls(self):
+    #    print("oooooooooh")
+    #    urls = super(TaskAdmin, self).get_urls()
+    #    my_urls = patterns('',
+    #                       ('^(?P<taskid>\d+)/$', self.change_view),
+    #                       ('^add/$', self.add_view))
+    #    return my_urls + urls
 
 
 class TaskCollectionAdmin(admin.ModelAdmin):
