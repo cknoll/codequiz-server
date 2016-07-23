0a2b61e866f1657d632087216d29748a882ee296
cknoll <CarstenKnoll@gmx.de>
2016-07-23 07:55:22 +0200

    remove an old url-pattern and update outdated-url-tests

--- codequiz-server/quiz/tests.py
+++ codequiz-server/quiz/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase  # super important to load TestCase like this! fixtures won't load otherwise
 from django.test.client import Client
 
+from IPython import embed as IPS
+
 
 class URLTest(TestCase):
     fixtures = ['testdata.json']
@@ -13,22 +15,23 @@ class URLTest(TestCase):
         response = self.client.get("/quiz/")
         self.assertEqual(response.status_code, 200)
 
-    def test_test_run_by_tc_id_and_tc_task_id(self):
+    def obsolete_test_test_run_by_tc_id_and_tc_task_id(self):
         response = self.client.get('/quiz/test/run/2/1/')
+        IPS()
         self.assertEqual(response.status_code, 200)
 
-    def test_result_tc_id_and_tc_task_id(self):
+    def test_leagacy_result_tc_id_and_tc_task_id(self):
         response = self.client.get('/quiz/test/result/1/0/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 404)
 
     def test_by_tc_id(self):
         response = self.client.get('/quiz/test/1/')
         self.assertEqual(response.status_code, 200)
 
-    def test_result_by_task_id(self):
-        # should fail
-        with self.assertRaises(ZeroDivisionError):
-            response = self.client.get('/quiz/result/1/')
+    def test_leagacy_result_by_task_id(self):
+        # test no old url scheme lurking arround
+        response = self.client.get('/quiz/result/1/')
+        self.assertEqual(response.status_code, 404)
 
     def test_explicit_by_task_id(self):
         response = self.client.get('/quiz/explicit/12/')
--- codequiz-server/quiz/urls.py
+++ codequiz-server/quiz/urls.py
@@ -8,10 +8,12 @@ urlpatterns = patterns('',
                        url(r'^debug$', views.template_debug, name='debug'),
                        url(r'^about$', views.simple, kwargs={"template": "about.html"}, name='about'),
                        url(r'^test/run/', views.tc_run_view, name='tc_run_view'),
-                       url(r'^test/result/(?P<tc_id>\d+)/(?P<tc_task_id>\d+)/$',
-                           views.tc_run_form_process, name='tc_run_form_process'),
                        url(r'^test/(?P<tc_id>\d+)/$', views.task_collection_view, name='task_collection_view'),
                        # view for showing a specific task outside of a tc
                        # TODO: this should be restricted to moderators
                        url(r'^explicit/(?P<task_id>\d+)/$', views.explicit_task_view, name='explicit_task_view'),
                        )
+
+# old advanced pattern:
+# url(r'^test/result/(?P<tc_id>\d+)/(?P<tc_task_id>\d+)/$',
+#                           views.tc_run_form_process, name='tc_run_form_process'),
