6339dceb029495432a36fa17c0f1d827240c1fae
Carsten Knoll <carsten.knoll@posteo.de>
2023-04-16 14:24:04 +0200

    fix urls #2

--- apps/quiz/urls.py
+++ apps/quiz/urls.py
@@ -1,20 +1,18 @@
-# -*- coding: utf-8 -*-
-
-from django.conf.urls import url
+from django.urls import re_path
 from quiz import views
 
-urlpatterns = [url(r'^$', views.simple, kwargs={"template": "index.html"}, name='index'),
-               url(r'^debug$', views.template_debug, name='debug'),
-               url(r'^about$', views.simple, kwargs={"template": "about.html"}, name='about'),
-               url(r'^test/run/', views.tc_run_view, name='tc_run_view'),
-               url(r'^test/(?P<tc_id>\d+)/$', views.task_collection_view,
+urlpatterns = [re_path(r'^$', views.simple, kwargs={"template": "index.html"}, name='index'),
+               re_path(r'^debug$', views.template_debug, name='debug'),
+               re_path(r'^about$', views.simple, kwargs={"template": "about.html"}, name='about'),
+               re_path(r'^test/run/', views.tc_run_view, name='tc_run_view'),
+               re_path(r'^test/(?P<tc_id>\d+)/$', views.task_collection_view,
                    name='task_collection_view'),
                # view for showing a specific task outside of a tc
                # TODO: this should be restricted to moderators
-               url(r'^explicit/(?P<task_id>\d+)/$', views.explicit_task_view,
+               re_path(r'^explicit/(?P<task_id>\d+)/$', views.explicit_task_view,
                    name='explicit_task_view'),
                ]
 
 # old advanced pattern:
-# url(r'^test/result/(?P<tc_id>\d+)/(?P<tc_task_id>\d+)/$',
+# re_path(r'^test/result/(?P<tc_id>\d+)/(?P<tc_task_id>\d+)/$',
 #                           views.tc_run_form_process, name='tc_run_form_process'),
--- codequiz-server-project/urls.py
+++ codequiz-server-project/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, re_path
+from django.urls import include, re_path, path
 from django.contrib import admin
 import quiz.views as quiz_app_views
 from django.contrib import admin
@@ -7,21 +7,22 @@ admin.autodiscover()
 
 urlpatterns = [re_path(r'^$', quiz_app_views.index, name='index'),
             re_path(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
-            re_path(r'^quiz/', include('quiz.urls', namespace="quiz_ns")),
+            re_path(r'^quiz/', include(('quiz.urls', "quiz"), namespace="quiz_ns")),
             # temporarily excluded
 
-            # url(r'^feedback/', include('feedback.urls')),
+            # re_path(r'^feedback/', include('feedback.urls')),
 
-            # url(r'^polls/', include('polls.urls')), # from django example app
+            # re_path(r'^polls/', include('polls.urls')), # from django example app
 
             # Examples:
-            # url(r'^ratings/', include('ratings.urls')),
-            # url(r'^$', 'codequiz.views.home', name='home'),
-            # url(r'^codequiz/', include('codequiz.foo.urls')),
+            # re_path(r'^ratings/', include('ratings.urls')),
+            # re_path(r'^$', 'codequiz.views.home', name='home'),
+            # re_path(r'^codequiz/', include('codequiz.foo.urls')),
 
             # Uncomment the admin/doc line below to enable admin documentation:
-            # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+            # re_path(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
             # Uncomment the next line to enable the admin:
-            url(r'^admin/', include(admin.site.urls)),
+            path('admin/', admin.site.urls)
+            # re_path(r'^admin/', include(admin.site.urls)),
             ]
